75bb082807485db425a76489f5651bcc092c9b9a
James Anwyl <ja.anwyl@gmail.com>
2018-12-06 02:48:50 +0000

    initial commit

---
+++ includes/nav.php
@@ -0,0 +1,32 @@
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <!-- Add google analytics id here. e.g ?id=UA-123456789-0 -->
+    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id="></script> -->
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white mb-4 border-bottom">
+        <a href="index.php" title="Warrington Dog Walkers - Home"><img src="./img/logo_custom.jpg" class="navbar-brand" alt="Warrington Dog Walkers - Logo"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto ml-4">
+                <li class="nav-item">
+                    <a class="nav-link" href="./about_us.php" title="About Us">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./services.php" title="Services">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./contact_us.php" title="Contact Us">Contact Us</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li>
+                    <a class="nav-link" href="./contact_us.php" title="Contact Us"><i class="icofont-phone"></i> 07914 251511</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="mailto:warringtondogwalkers@gmail.com" title="Contact Us"><i class="icofont-envelope"></i> warringtondogwalkers@gmail.com</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
