035444ba6c986c626da3eede48c43426461cf24a
janwyl1 <ja.anwyl@gmail.com>
2020-01-09 18:04:38 +0000

    Removed out of date jquery and bootstrap versions from all.js. Used cdn links instead

--- build/includes/header.php
+++ build/includes/header.php
@@ -2,7 +2,7 @@
     // Set security headers 
 	header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
 	header("X-XSS-Protection: 1; mode=block");
-    header("Content-Security-Policy: default-src 'self'; script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' data: https://www.googletagmanager.com https://www.google-analytics.com; connect-src https://www.google-analytics.com
+    header("Content-Security-Policy: default-src 'self'; script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://code.jquery.com https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com; style-src 'self' 'unsafe-inline'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' data: https://www.googletagmanager.com https://www.google-analytics.com; connect-src https://www.google-analytics.com
     ");
 	header("X-Powered-By: tinyFluffyKittens");
 	header("X-Frame-Options: deny");
